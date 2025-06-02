e217af7935242068ad46100fa30af965c370c7a5
roshanprasad804 <82306250+roshanprasad804@users.noreply.github.com>
2025-06-02 21:19:17 +0530

    components and lazy routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    {
+        path: 'login',
+        loadChildren: () =>
+            import('./login/login.component').then((m) => m.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadChildren: () =>
+            import('./register/register.component').then((m) => m.RegisterComponent)
+    },
+    { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
