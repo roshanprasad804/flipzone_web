932d960c0b7b06b214dd40ebfb146524e7d353cf
roshanprasad804 <82306250+roshanprasad804@users.noreply.github.com>
2025-06-02 22:01:22 +0530

    home component nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: '', component: HomeComponent},
     {
         path: 'login',
-        loadChildren: () =>
+        loadComponent: () =>
             import('./login/login.component').then((m) => m.LoginComponent)
     },
     {
         path: 'register',
-        loadChildren: () =>
+        loadComponent: () =>
             import('./register/register.component').then((m) => m.RegisterComponent)
     },
-    { path: '**', component: HomeComponent, pathMatch: 'full' },
+    { path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatButtonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
