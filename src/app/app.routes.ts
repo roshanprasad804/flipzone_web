a0b5dfe7f72f56aad3f6d6f20c34ee7f865ca62d
roshanprasad804 <82306250+roshanprasad804@users.noreply.github.com>
2025-06-16 11:43:08 +0530

    home page basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
-    {
-        path: 'login',
-        loadComponent: () =>
-            import('./login/login.component').then((m) => m.LoginComponent)
-    },
-    {
-        path: 'register',
-        loadComponent: () =>
-            import('./register/register.component').then((m) => m.RegisterComponent)
-    },
-    { path: '**', component: HomeComponent, pathMatch: 'full'},
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register.component').then((m) => m.RegisterComponent)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,47 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule,MatButtonModule, RouterModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  images = [
+    'assets/img1.jpg',
+    'assets/img2.jpg',
+    'assets/img3.jpg'
+  ];
+  currentIndex = 0;
+  intervalId: any;
 
+  ngOnInit() {
+    this.intervalId = setInterval(() => this.next(), 3000); // auto-slide every 3 seconds
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
